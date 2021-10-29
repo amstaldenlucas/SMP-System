6e941e1687d408036c9db0957ad740846d6db0b6
AMSTALDEN <lucas-amstalden@hotmail.com>
2021-10-29 17:45:28 -0300

    added model

---
+++ SMPSystem/Models/ProductionOrder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPSystem.Models
+{
+    public class ProductionOrder
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public DateTime PrevisionDate { get; set; }
+        public bool FinalizedStatus { get; set; }
+        public int TotalQuantity { get; set; }
+        public int ProducedQuantity { get; set; }
+        public int LostQuantity { get; set; }
+
+        public int DbUserId { get; set; }
+        public DbUser DbUser { get; set; }
+
+
+
+    }
+}
