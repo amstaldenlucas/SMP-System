65b4709939d4a3b330c7cda1d6def4a69ce64115
Amstalden <lucas-amstalden@hotmail.com>
2021-10-31 17:07:37 -0300

    added controller to user operate the system

---
+++ SMPSystem/Areas/Web/Controllers/UserProductionHistoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SMPSystem.Areas.Web.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMPSystem.Areas.Web.Controllers
+{
+    [Authorize]
+    [Area("Web")]
+    public class UserProductionHistoryController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+            // ProductionorderId
+            return View();
+        }
+    }
+}
