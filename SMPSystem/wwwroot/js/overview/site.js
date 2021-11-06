448803888e37ceed2ba29f5ff9048e80a6c4f7f0
Amstalden <lucas-amstalden@hotmail.com>
2021-11-06 16:21:03 -0300

    fixed layout

---
+++ SMPSystem/wwwroot/js/overview/site.js
@@ -0,0 +1,15 @@
+﻿function BoxConfirm(boxmessage, uri, event) {
+    event.preventDefault();
+    bootbox.confirm({
+        closeButton: false,
+        message: boxmessage,
+        className: '',
+        buttons: {
+            confirm: { label: 'Sim', className: 'btn-primary' },
+            cancel: { label: 'Não', className: 'c-btn-secondary' }
+        },
+        callback: function (result) {
+            if (result) location.href = uri;
+        }
+    });
+}
